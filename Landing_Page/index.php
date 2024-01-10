bd3187c7d922ff3fad8b65e40e5abaf246f0dd50
Nidhi Shetty <herganidhi06@gmail.com>
2024-01-10 12:37:46 +0530

    files added

---
+++ Landing_Page/index.php
@@ -0,0 +1,154 @@
+<!Doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+	<link rel="stylesheet" href="css/style.css">
+</head>
+  <body>
+    <!-- Navigation Bar Starts -->
+   <nav class="navbar navbar-expand-lg py-3 sticky-top navbar-light bg-white">
+  		<div class="container">
+    		<a class="navbar-brand" href="#">
+        		<img class="logo" src="img/images (8).jpeg" alt="">
+    		</a>
+    		<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      			<span class="navbar-toggler-icon"></span>
+    		</button>
+    		<div class="collapse navbar-collapse" id="navbarNav">
+      			<ul class="navbar-nav ms-auto"> 
+        			<li class="nav-item">
+          				<a class="nav-link" href="#home">Home</a>
+        			</li>
+        			<li class="nav-item">
+          				<a class="nav-link" href="#services">Services</a>
+        			</li>
+        			<li class="nav-item">
+          				<a class="nav-link" href="#">Fetures</a>
+        			</li>
+        			<li class="nav-item">
+          				<a class="nav-link" href="#">Portfolio</a>
+        			</li>
+        			<li class="nav-item">
+          				<a class="nav-link" href="#">Pricing</a>
+        			</li>
+        			<li class="nav-item">
+          				<a class="nav-link" href="#">Team</a>
+        			</li>        
+        			<li class="nav-item">
+          				<a class="nav-link" href="#">Blog</a>
+        			</li>
+        			<li class="nav-item">
+          				<a class="nav-link" href="#">Contact</a>
+        			</li>
+      			</ul>
+      			<button class="btn btn-primary ms-lg-3">Join Us</button>
+    		</div>
+  		</div>
+	</nav>
+<!-- Navigation Bar Ends -->
+<!-- Hero starts -->
+<div class="hero vh-100 d-flex align-items-center" id="home">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-7 mx-auto text-center">
+                <h1 class="display-4 text-white">Build Robust Landing Page Now</h1>
+                <p class="text-white my-4">A startup company is a marketing tool that showcases your brand's products, services and activities in a general company overview.A compelling company profile presentation should be more than just a brochure.It vibrantly shoecases your bussines and engages with stakeholders about the company's offering and unique qualities.</p>
+                <a href="#" class="btn me-2 btn-primary">Get Started</a>
+                <a href="#" class="btn btn-secondary">My Portfolio</a>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Hero ends -->
+<!-- Services Starts -->
+<section id="services">
+	<div class="container">
+		<div class="row mb-5">
+			<div class="col-md-8 mx-auto text-center">
+				<h6 class="text-primary">SERVICES</h6>
+				<h1>Our Services</h1>
+				<p>It creates an opportunity for collaboration. Your profile acts like a dossier that hightlights the company history, philosophy, future plans and growth roadmap of your bussiness.Stakeholders can decide from it if you are the right fit and possess the same bussiness ethics that align with their own goals.</p>
+			</div>
+		</div>
+		<div class="row g-4">
+			<div class="col-lg-4 col-sm-6">
+				<div class="service card-effect">
+					<div class="iconbox">
+						<i class='bx bxs-comment-detail'></i>
+					</div>
+					<h5 class="mt-4 mb-2">Web Development</h5>
+					<p>Web development refers to the creating, building and maintaining of websites. It includes aspects such as web design, web publishing, web programming and database management.</p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-sm-6">
+				<div class="service card-effect">
+					<div class="iconbox">
+						<i class='bx bx-font-family'></i>
+					</div>
+					<h5 class="mt-4 mb-2">Digital Marketing</h5>
+					<p>Digital marketing is the component of marketing that uses the Internet and online-based digital technologies such as desktop computers, mobile phones and other digital media and platforms. </p>
+				</div>
+			</div>
+			<div class="col-lg-4 col-sm-6">
+				<div class="service card-effect">
+					<div class="iconbox">
+						<i class='bx bxl-wordpress'></i>
+					</div>
+					<h5 class="mt-4 mb-2">Mobile Application development</h5>
+					<p>Mobile Application development is the process of making software for smartphones, tablets and digital asistants, most commonly for the Android and iOS operating system.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Services Ends -->
+<!-- Features Starts -->
+<section class="row w-100" id="features">
+	<div class="col-lg-6 col-img"></div>
+	<div class="col-lg-6">
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10 offset-md-1">
+					<h6 class="text-primary">Why to choose us?</h6>
+					<h1>Best Solution for your bussiness</h1>
+					<p>It creates an opportunity for collaboration. Your profile acts like a dossier that hightlights the company history, philosophy, future plans and growth roadmap of your bussiness.Stakeholders can decide from it if you are the right fit and possess the same bussiness ethics that align with their own goals.</p>
+					<div class="feature d-flex mt-5">
+						<div class="iconbox me-3">
+							<i class='bx bxs-comment-detail'></i>
+						</div>
+						<div>
+							<h5>Features</h5>
+							<p>It creates an opportunity for collaboration. Your profile acts like a dossier that hightlights the company history, philosophy, future plans and growth roadmap of your bussiness.</p>
+						</div>
+					</div>
+					<div class="feature d-flex">
+						<div class="iconbox me-3">
+							<i class='bx bxs-comment-detail'></i>
+						</div>
+						<div>
+							<h5>Features</h5>
+							<p>It creates an opportunity for collaboration. Your profile acts like a dossier that hightlights the company history, philosophy, future plans and growth roadmap of your bussiness.</p>
+						</div>
+					</div>
+					<div class="feature d-flex">
+						<div class="iconbox me-3">
+							<i class='bx bxs-comment-detail'></i>
+						</div>
+						<div>
+							<h5>Features</h5>
+							<p>It creates an opportunity for collaboration. Your profile acts like a dossier that hightlights the company history, philosophy, future plans and growth roadmap of your bussiness.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+<!-- Features Ends -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
---
+++ Tribute_Page/index.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tribute Page</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css">
+</head>
+<body>
+    <section id="heading">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 text-center py-4">
+                    <img src="1644211468700.jpg" alt="Lata Mangeshkar" width="300px" height="300px">
+                </div>
+                <div class="col-md-6">
+                    <p class="promo-title text-center">Lata Mangeshkar</p>
+                    <p>Nightingale of India Lata Mangeshkar (28 September 1929 – 6 Febuary 2022) was an Indian playback singer and occasional music composer.She is considered to be the greatest and most influential singers of the Indian subcontinent</p>
+                    <div class="text-center">
+                        <a href="#more" class="btn btn-primary">READ MORE</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- Life -->
+    <section id="life">
+        <div class="container" id="more">
+            <h3 class="title text-center font-weight-bold">Early Life And Education</h3>
+            <ul class="py-4">
+            <li>1) Mageshkar was born in a Brahmin family in Indore of Madhya Pradesh, then the capital of the princely state of Indore which was part of the Central India Agency in British India .</li>
+                <li>2) She was the eldest child in the family. Meena, Asha, Usha, Hridayanth are her siblings, all are accomplished singers and muscians.</li>
+                <li>3) She recieved her first music lesson from her father.At the age of five, she started to work as an actoress in her father's musical plays(Sangeet Natak in Marathi).</li>
+            </ul>
+        </div>
+    </section>
+    <section id="life">
+        <div class="container" id="more">
+            <h3 class="title text-center font-weight-bold">National Recognition</h3>
+            <ul class="py-4">
+                <li>1) "Dil Mera Toda, Mujhe Kahin Ka Na Chhora"- lyrics by Nazim Panipati in the movie Majboor(1948) became her big breakthrough film hit.</li>
+                <li>2) In 1973, she won the National Film Award for Best Female Playback Singer for the the song "Beeti Na Bitai" from the film Parichay, composed by R.D.Burman and written by Gulzar.</li>
+                <li>3) In 2001, Mangeshkar was awarded the Bharat Ratna, India's Highest civilian award.</li>
+            </ul>
+        </div>
+    </section>
+    <section id="life">
+        <div class="container" id="more">
+            <h3 class="title text-center font-weight-bold">Collaborations with other singers</h3>
+            <ul class="py-4">
+                <li>1) From the 1940s to 1970s Mangeshkar sand duets with Asha Bhosale, Suraiya, Shamshad Begaum and Mahendra Kapoor.</li>
+                <li>2) In the 2000s her duets were performed with Udit Narayan and Sonu Nigam.</li>
+                <li>3) 2005-06 were the years of her last well-known songs: "Kaise Piya Se" from Bewafa(2005) and "Lukka Chhupi" in Rang De Basanti(2006 film) with A R Rahman.</li>
+                <li>4) One of her latest songs was "Jeena Hai Kya" from Dunno Y2(2015).</li>
+            </ul>
+        </div>
+    </section>
+    <section id="life">
+        <div class="container" id="more">
+            <h3 class="title text-center font-weight-bold">Non-Singing Career</h3>
+            <ul class="py-4">
+                <li>1) Lata Mangeshkar composed music for the first time in 1955 for the Marathi movie Ram Ram Pavhane</li>
+                <li>2) She composed music for the following Marathi movies under the pseudonym of Anand Ghan</li>
+                <li>* 1950 - Maratha Tituka Melvava</li>
+                <li>* 1963 - Mohityanchi Manjula</li>
+                <li>* 1965 - Sandhi Manase</li>
+                <li>* 1969 - Tambadi Mati</li>
+            </ul>
+        </div>
+    </section>
+    <section id="life">
+        <div class="container" id="more">
+            <h3 class="title text-center font-weight-bold">Illness and Death</h3>
+            <ul class="py-4">
+                <li>1) On 8 January 2022, Mangeshkar tested positive for Covid-19 with mild symtoms and was admitted to Breach Candy Hospital's Intensive Care Unit(ICU).</li>
+                <li>2) Mangeshkar died from multiple organ dysfunction syndrome on 6 Febuary 2022, at the age of 92. She had undergone 28 days of constant treatment for pneumonia and Covid-19.</li>
+            </ul>
+        </div>
+    </section>
+    <!-- Awards -->
+    <section id="life">
+        <div class="container" id="more">
+            <h3 class="title text-center font-weight-bold">Awards</h3>
+            <div class="col-md-12 py-4 table-respo">
+                <table class="table table-bordered">
+                    <thead class="thead-dark">
+                        <tr>
+                            <th scope="col">YEAR</th>
+                            <th scope="col">FILM</th>
+                            <th scope="col">AWARD</th>
+                            <th scope="col">SONG</th>
+                            <th scope="col">RESULT</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                        <tr>
+                              <th scope="row">1962</th>
+                              <td>Anpadth</td>
+                              <td>Filmfare Awards</td>
+                              <td>Lag Jaa Gale</td>
+                              <td>WON</td>
+                            </tr>
+                        </tr>
+                        <tr>
+                                  <th scope="row">1966</th>
+                                  <td>Mera Saya</td>
+                                  <td>Filmfare Awards</td>
+                                  <td>Tu Jahan Jahan Chalega</td>
+                                  <td>WON</td>
+                            </tr>
+                            <tr>
+                                  <th scope="row">1973</th>
+                                  <td>Parichay</td>
+                                  <td>National Film Award</td>
+                                  <td>Beeti Na Bitai</td>
+                                  <td>WON</td>
+                            </tr>
+                            <tr>
+                                  <th scope="row">1978</th>
+                                  <td>Satyam Shivam Sundaram</td>
+                                  <td>National Film Awards</td>
+                                  <td>Main Theme-Satyam Shivam Sundaram</td>
+                                  <td>WON</td>
+                            </tr>
+                            <tr>
+                                  <th scope="row">1989</th>
+                                  <td>Waaris</td>
+                                  <td>Asian Film Awards</td>
+                                  <td>Mere Pyar ki Umar</td>
+                                  <td>WON</td>
+                            </tr>
+                            <tr>
+                                  <th scope="row">1990</th>
+                                  <td>Lekin</td>
+                                  <td>National Film Award</td>
+                                  <td>Yaara Sili Sili</td>
+                                  <td>WON</td>
+                            </tr>
+                            <tr>
+                                  <th scope="row">2001</th>
+                                  <td>-----</td>
+                                  <td>Bharat Ratna</td>
+                                  <td>Arts</td>
+                                  <td>WON</td>
+                            </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </section>
+    <marquee direction="left">
+        <strong>THE GREAT SINGER LATA MANGESHKAR LEFT US ALONE</strong>
+    </marquee>
+    <hr>
+    <footer>
+        <p class="text-center font-weight-bold">All Rights Reserved &copy; Nidhi Shetty</p>
+    </footer>
+</body>
+</html>
